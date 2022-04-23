a56e71bd4b0d3115e686fd5c08b7ab89a6de082a
Luke Patterson <lukepatterson31@gmail.com>
2022-04-23 16:46:09 +0100

    added tcpServer

--- tcpClient.py
+++ tcpClient.py
@@ -3,12 +3,13 @@ import socket
 target_host = "www.google.com"
 target_port = 80
 target_address = (target_host, target_port)
+request = b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n"
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect(target_address)
 
-client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+client.send(request)
 
 response = client.recv(4096)
 
